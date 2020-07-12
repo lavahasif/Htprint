3950dfac34eca5c1a87f5d6119a17afbbc257303
Hasif <lavahasif@gmail.com>
2020-07-12 22:42:13 +0530

    parameter added save kotprint(48,map,list)

--- src/main/java/com/example/htprint/Constants.java
+++ src/main/java/com/example/htprint/Constants.java
@@ -3,12 +3,12 @@ package com.example.htprint;
 public class Constants {
 
 
-    static int HEAD_BILLNO_0 = 0;
-    static int HEAD_CASHBILL_1 = 1;
-    static int HEAD_DATE_2 = 2;
-    static int TIME_3 = 3;
-    static int WAITER_4 = 4;
-    static int TABLE_5 = 5;
-    static int IMAGE_6 = 6;
-    static int KOTNO_7 = 7;
+    public static int HEAD_BILLNO_0 = 0;
+    public static int HEAD_CASHBILL_1 = 1;
+    public static int HEAD_DATE_2 = 2;
+    public static int TIME_3 = 3;
+    public static int WAITER_4 = 4;
+    public static int TABLE_5 = 5;
+    public static int IMAGE_6 = 6;
+    public static int KOTNO_7 = 7;
 }
